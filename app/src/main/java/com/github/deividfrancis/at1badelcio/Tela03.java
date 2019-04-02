8d29acb60ad1ae7952ac8e195a5d69d481865f4d
DeividF <deividfrancisdeoliveira@gmail.com>
2019-04-02 02:15:51 -0400

    Projeto finalizado (Sem revisao)

--- app/src/main/java/com/github/deividfrancis/at1badelcio/Tela03.java
+++ app/src/main/java/com/github/deividfrancis/at1badelcio/Tela03.java
@@ -1,7 +1,23 @@
 package com.github.deividfrancis.at1badelcio;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.MaskFilter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.deividfrancis.at1badelcio.core.AppCompatActivityDefault;
 
@@ -9,6 +25,9 @@ public class Tela03 extends AppCompatActivityDefault {
 
 
     TextView txt;
+    EditText edtText;
+    int radioId;
+    ConstraintLayout formLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +39,87 @@ public class Tela03 extends AppCompatActivityDefault {
         txt = findViewById(R.id.txt);
         txt.setText("Bem vindo a tela 03, Caso queira mudar esta mensagem vá ate a tela 02 e digite algo.");
 
+        edtText = findViewById(R.id.edtTxt);
+        formLayout = findViewById(R.id.formLayoutId);
+        formLayout.setVisibility(View.INVISIBLE);
+
         loadBundle();
     }
 
+    public void onCheck(View view) {
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioId = radioGroup.getCheckedRadioButtonId();
 
-    private void loadBundle(){
+        boolean isChecked = ((RadioButton) view).isChecked();
+        if (isChecked) {
+            formLayout.setVisibility(View.VISIBLE);
+        }
+
+        switch (radioId) {
+            case R.id.radioWeb:
+                edtText.setHint("Digite uma pesquisa");
+                edtText.setInputType(InputType.TYPE_CLASS_TEXT);
+                break;
+            case R.id.radioPhone:
+                edtText.setHint("Digite um telefone");
+                edtText.setInputType(InputType.TYPE_CLASS_PHONE);
+                break;
+            case R.id.radioMaps:
+                edtText.setHint("Digite um endereço");
+                edtText.setInputType(InputType.TYPE_CLASS_TEXT);
+                break;
+
+        }
+    }
+
+    public void onClick(View view) {
+
+        String edtTextStr = edtText.getText().toString();
+        String msg = null;
+        String uriText = null;
+        String itAction = null;
+        Intent intent = new Intent();
+
+        switch (view.getId()) {
+            case R.id.btnEnviar:
+                switch (radioId) {
+                    case R.id.radioWeb:
+                        itAction = Intent.ACTION_VIEW;
+                        uriText = "https://www.google.com/search?q=" + edtTextStr;
+                        break;
+                    case R.id.radioPhone:
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            msg = "Você não tem permição boco!";
+                            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                        itAction = Intent.ACTION_CALL;
+                        uriText = "tel:" + edtTextStr.replaceAll("[^0-9]", "");
+                        break;
+                    case R.id.radioMaps:
+                        itAction = Intent.ACTION_VIEW;
+                        intent.setPackage("com.google.android.maps");
+                        uriText = "geo:0,0?q=" + edtTextStr;
+                        break;
+                    default:
+                        msg = "Selecione uma opção antes de clicar!";
+                        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+                        return;
+
+                }
+                break;
+        }
+
+        intent = new Intent(itAction, Uri.parse(uriText));
+        startActivity(intent);
+    }
+
+    private void loadBundle() {
         Bundle bundle = getIntent().getExtras();
 
-        if(bundle == null) return;
+        if (bundle == null) return;
 
-        if(bundle.containsKey("edtMensagem")){
+        if (bundle.containsKey("edtMensagem")) {
             txt.setText(bundle.getString("edtMensagem"));
         }
     }
--- app/src/main/java/com/github/deividfrancis/at1badelcio/core/AppCompatActivityDefault.java
+++ app/src/main/java/com/github/deividfrancis/at1badelcio/core/AppCompatActivityDefault.java
@@ -2,12 +2,14 @@ package com.github.deividfrancis.at1badelcio.core;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.github.deividfrancis.at1badelcio.MainActivity;
 import com.github.deividfrancis.at1badelcio.R;
 
 public class AppCompatActivityDefault extends AppCompatActivity {
@@ -15,9 +17,17 @@ public class AppCompatActivityDefault extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
+        boolean isMainActivity = false;
+
+        if(this.getClass().equals(MainActivity.class) == false){
+            isMainActivity = true;
+        }
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(isMainActivity); //Mostrar o botão
+        getSupportActionBar().setHomeButtonEnabled(isMainActivity);
+
         // Setar na tela
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -26,15 +36,18 @@ public class AppCompatActivityDefault extends AppCompatActivity {
 
         String text = null;
         switch (item.getItemId()) {
-            case R.id.item_voltar:
-                text = "Vou voltar";
-                break;
+//            case R.id.item_voltar:
+//                text = "Vou voltar";
+//                break;
             case R.id.item_sair:
                 alertDialogExit();
                 break;
+            case android.R.id.home:
+                finish();
+                break;
         }
-        if(text != null)
-        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        if (text != null)
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
 
         return super.onOptionsItemSelected(item);
     }
@@ -52,7 +65,7 @@ public class AppCompatActivityDefault extends AppCompatActivity {
         builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText( obj, "Uffa!", Toast.LENGTH_LONG).show();
+                Toast.makeText(obj, "Uffa!", Toast.LENGTH_LONG).show();
             }
         });
 
